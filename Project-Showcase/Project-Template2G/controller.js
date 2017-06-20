61f57eeb22d5f8e49bc0791bbd9f9924135c511c
mahfuz_aftab <mahfuz.cracker@gmail.com>
2017-06-20 13:04:25 +0600

    Feat: Slick-Slider with Floor Plan Connection

--- Project-Showcase/Project-Template2G/controller.js
+++ Project-Showcase/Project-Template2G/controller.js
@@ -6,161 +6,133 @@ app.controller('MainCtrl', function($scope) {
     $scope.floorPlanImageSelected = function(index) {
         console.log(index);
         $scope.selectedIndex = index;
+        changeTheSlide(index);
     }
     var slides = [{
             image: './img/1.jpg',
             text: "Bedroom1",
             top: "",
-            left: ""
+            left: "",
+            index_number: 0
         },
         {
             image: './img/2.jpg',
             text: "Bedroom2",
             top: "195px",
-            left: "165px"
+            left: "165px",
+            index_number: 1
         },
         {
             image: './img/3.jpg',
             text: "Master Bedroom",
             top: "120px",
-            left: "175px"
+            left: "175px",
+            index_number: 2
         },
         {
             image: './img/4.jpg',
             text: "Kitchen",
             top: "72px",
-            left: "228px"
+            left: "228px",
+            index_number: 3
         },
         {
             image: './img/5.jpg',
             text: "Dining Room",
             top: "140px",
-            left: "255px"
+            left: "255px",
+            index_number: 4
         },
         {
             image: './img/6.jpg',
             text: "Living Room",
             top: "285px",
-            left: "165px"
+            left: "165px",
+            index_number: 5
         },
         {
             image: './img/7.jpg',
             text: "Office Room",
             top: "420px",
-            left: "240px"
+            left: "240px",
+            index_number: 6
         },
         {
             image: './img/8.jpg',
             text: "Entry",
             top: "290px",
-            left: "265px"
+            left: "265px",
+            index_number: 7
         },
         {
             image: './img/9.jpg',
             text: "Street Gate",
             top: "",
-            left: ""
+            left: "",
+            index_number: 8
         },
         {
             image: './img/10.jpg',
             text: "Park Lane",
             top: "",
-            left: ""
+            left: "",
+            index_number: 9
         },
         {
             image: './img/11.jpg',
             text: "Street Gate",
             top: "",
-            left: ""
+            left: "",
+            index_number: 10
         },
         {
             image: './img/12.jpg',
             text: "Shoals Granite",
             top: "",
-            left: ""
+            left: "",
+            index_number: 11
         }
     ];
-
-    jssor_1_slider_init = function() {
-
-          var jssor_1_SlideshowTransitions = [
-            {$Duration:1200,x:0.3,$During:{$Left:[0.3,0.7]},$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-            {$Duration:1200,x:-0.3,$SlideOut:true,$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-            {$Duration:1200,x:-0.3,$During:{$Left:[0.3,0.7]},$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-            {$Duration:1200,x:0.3,$SlideOut:true,$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-            {$Duration:1200,y:0.3,$During:{$Top:[0.3,0.7]},$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-            {$Duration:1200,y:-0.3,$SlideOut:true,$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-            {$Duration:1200,y:-0.3,$During:{$Top:[0.3,0.7]},$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-            {$Duration:1200,y:0.3,$SlideOut:true,$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-            {$Duration:1200,x:0.3,$Cols:2,$During:{$Left:[0.3,0.7]},$ChessMode:{$Column:3},$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-            {$Duration:1200,x:0.3,$Cols:2,$SlideOut:true,$ChessMode:{$Column:3},$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-            {$Duration:1200,y:0.3,$Rows:2,$During:{$Top:[0.3,0.7]},$ChessMode:{$Row:12},$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-            {$Duration:1200,y:0.3,$Rows:2,$SlideOut:true,$ChessMode:{$Row:12},$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-            {$Duration:1200,y:0.3,$Cols:2,$During:{$Top:[0.3,0.7]},$ChessMode:{$Column:12},$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-            {$Duration:1200,y:-0.3,$Cols:2,$SlideOut:true,$ChessMode:{$Column:12},$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-            {$Duration:1200,x:0.3,$Rows:2,$During:{$Left:[0.3,0.7]},$ChessMode:{$Row:3},$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-            {$Duration:1200,x:-0.3,$Rows:2,$SlideOut:true,$ChessMode:{$Row:3},$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-            {$Duration:1200,x:0.3,y:0.3,$Cols:2,$Rows:2,$During:{$Left:[0.3,0.7],$Top:[0.3,0.7]},$ChessMode:{$Column:3,$Row:12},$Easing:{$Left:$Jease$.$InCubic,$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-            {$Duration:1200,x:0.3,y:0.3,$Cols:2,$Rows:2,$During:{$Left:[0.3,0.7],$Top:[0.3,0.7]},$SlideOut:true,$ChessMode:{$Column:3,$Row:12},$Easing:{$Left:$Jease$.$InCubic,$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-            {$Duration:1200,$Delay:20,$Clip:3,$Assembly:260,$Easing:{$Clip:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-            {$Duration:1200,$Delay:20,$Clip:3,$SlideOut:true,$Assembly:260,$Easing:{$Clip:$Jease$.$OutCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-            {$Duration:1200,$Delay:20,$Clip:12,$Assembly:260,$Easing:{$Clip:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-            {$Duration:1200,$Delay:20,$Clip:12,$SlideOut:true,$Assembly:260,$Easing:{$Clip:$Jease$.$OutCubic,$Opacity:$Jease$.$Linear},$Opacity:2}
-          ];
-
-          var jssor_1_options = {
-            $AutoPlay: 1,
-            // $SlideWidth: 2000, //assign width
-            // $SlideHeight: 1500, //assign height
-            $SlideshowOptions: {
-              $Class: $JssorSlideshowRunner$,
-              $Transitions: jssor_1_SlideshowTransitions,
-              $TransitionsOrder: 1
-            },
-            $ArrowNavigatorOptions: {
-              $Class: $JssorArrowNavigator$
-            },
-            $ThumbnailNavigatorOptions: {
-              $Class: $JssorThumbnailNavigator$,
-              $Cols: 10,
-              $SpacingX: 8,
-              $SpacingY: 8,
-              $Align: 360
-            }
-          };
-
-          var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
-
-          /*responsive code begin*/
-          /*remove responsive code if you don't want the slider scales while window resizing*/
-          function ScaleSlider() {
-              var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
-              if (refSize) {
-                  refSize = Math.min(refSize, 800);
-                  jssor_1_slider.$ScaleWidth(refSize);
-              }
-              else {
-                  window.setTimeout(ScaleSlider, 30);
-              }
-          }
-          ScaleSlider();
-          $Jssor$.$AddEvent(window, "load", ScaleSlider);
-          $Jssor$.$AddEvent(window, "resize", ScaleSlider);
-          $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
-          /*responsive code end*/
-      };
 
     $scope.slides = slides;
 
     var inHTML = "";
     $.each(slides, function(index, value) {
-        var newItem = "<div><img data-u='image' src =" + value.image +" /> <img data-u='thumb' src =" + value.image +" /></div>"
+        var newItem = "<div><img class=" + value.index_number + " src =" + value.image + "></div>"
         inHTML += newItem;
     });
+
+    var bgImgHTML = "";
+    $.each(slides, function(index, value) {
+        var bgNewItem = "<div><img class=" + value.index_number + " src =" + value.image + "></div>"
+
+        bgImgHTML += bgNewItem;
+    });
+    $("#dynamicBG").html(inHTML);
 
     $("#dynamicThumbs").html(inHTML);
 
     $(document).on('ready', function() {
-      jssor_1_slider_init();
+        $('.slider-for').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            fade: true,
+            asNavFor: '.slider-nav'
+        });
+        $('.slider-nav').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            asNavFor: '.slider-for',
+            dots: false,
+            centerMode: true,
+            focusOnSelect: true
+        });
+
     });
+    var changeTheSlide = function(index) {
+        var clickedImageIndex = $("#dynamicThumbs ." + index).first().parent().attr('data-slick-index');
+        console.log(clickedImageIndex);
+        $('.slider-nav').slick('slickGoTo', clickedImageIndex);
+    }
 });
