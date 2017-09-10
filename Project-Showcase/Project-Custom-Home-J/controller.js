1074d6848e1beaf033e49f73aa9c5e96707dcd8d
mahfuz_aftab <mahfuz.cracker@gmail.com>
2017-09-10 19:04:13 +0600

    WIP

--- Project-Showcase/Project-Custom-Home-J/controller.js
+++ Project-Showcase/Project-Custom-Home-J/controller.js
@@ -73,4 +73,60 @@ app.controller('MainCtrl', function($scope) {
         }
     ];
 
+    $scope.metalRoofColors = [{
+            name: "Dark Slate Gray",
+            color: "#2F4F4F"
+        },
+        {
+            name: "Gray",
+            color: "#ccc"
+        },
+        {
+            name: "Red",
+            color: "#FF0000"
+        },
+        {
+            name: "Orange Red",
+            color: "#FF4500"
+        },
+        {
+            name: "Navy",
+            color: "#000080"
+        }
+    ];
+
+    $scope.floorPlans = [{
+            name: "8x8 Shed",
+            img: "./img/fp/sheds-8x8.jpg"
+        },
+        {
+            name: "8x12 Shed",
+            img: "./img/fp/sheds-8x12.jpg"
+        },
+        {
+            name: "10x12 Shed",
+            img: "./img/fp/sheds-10x12.jpg"
+        },
+        {
+            name: "10x16 Shed",
+            img: "./img/fp/sheds-10x16.jpg"
+        },
+        {
+            name: "12x16 Shed",
+            img: "./img/fp/sheds-12x16.jpg"
+        },
+        {
+            name: "12x24 Shed",
+            img: "./img/fp/sheds-12x24.jpg"
+        },
+        {
+            name: "12x20 Shed",
+            img: "./img/fp/sheds-12x20.jpg"
+        },
+        {
+            name: "14x28 Shed",
+            img: "./img/fp/sheds-14x28.jpg"
+        }
+    ];
+
 });
