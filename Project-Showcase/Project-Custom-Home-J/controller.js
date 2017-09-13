8bd17af0cd3ebe52d2201e9f6ff237daf9aaf73b
mahfuz_aftab <mahfuz.cracker@gmail.com>
2017-09-13 18:02:37 +0600

    Feat: Layout Done

--- Project-Showcase/Project-Custom-Home-J/controller.js
+++ Project-Showcase/Project-Custom-Home-J/controller.js
@@ -17,7 +17,8 @@ app.controller('MainCtrl', function($scope) {
         {
             name: "The Lincoln",
             image: "./img/h4.jpg"
-        }, {
+        },
+        {
             name: "The Standard",
             image: "./img/h5.jpg"
         },
@@ -97,36 +98,58 @@ app.controller('MainCtrl', function($scope) {
 
     $scope.floorPlans = [{
             name: "8x8 Shed",
-            img: "./img/fp/sheds-8x8.jpg"
+            img: "./img/fp/sheds-8x8.jpg",
+            dt: "fp1"
         },
         {
             name: "8x12 Shed",
-            img: "./img/fp/sheds-8x12.jpg"
+            img: "./img/fp/sheds-8x12.jpg",
+            dt: "fp2"
         },
         {
             name: "10x12 Shed",
-            img: "./img/fp/sheds-10x12.jpg"
+            img: "./img/fp/sheds-10x12.jpg",
+            dt: "fp3"
         },
         {
             name: "10x16 Shed",
-            img: "./img/fp/sheds-10x16.jpg"
+            img: "./img/fp/sheds-10x16.jpg",
+            dt: "fp4"
         },
         {
             name: "12x16 Shed",
-            img: "./img/fp/sheds-12x16.jpg"
+            img: "./img/fp/sheds-12x16.jpg",
+            dt: "fp5"
         },
         {
             name: "12x24 Shed",
-            img: "./img/fp/sheds-12x24.jpg"
+            img: "./img/fp/sheds-12x24.jpg",
+            dt: "fp6"
         },
         {
             name: "12x20 Shed",
-            img: "./img/fp/sheds-12x20.jpg"
+            img: "./img/fp/sheds-12x20.jpg",
+            dt: "fp7"
         },
         {
             name: "14x28 Shed",
-            img: "./img/fp/sheds-14x28.jpg"
+            img: "./img/fp/sheds-14x28.jpg",
+            dt: "fp8"
         }
     ];
+
+    $scope.quesAnss = [{
+        q: "DO I NEED A PERMIT FOR MY SHED OR GARAGE?",
+        collapseVal: "collapse-6",
+        a: "That varies by town and municipality. Check with your local code office to see what the requirements are for your location. They will provide you with an application for a permit if needed and let you know the process you need to take. If you have questions about the size and details of your structure, contact your Woodtex Sales Advisor."
+    }, {
+        q: "HOW LONG WILL IT TAKE FOR ME TO GET MY SHED?",
+        collapseVal: "collapse-7",
+        a: "Timeframe varies on location, time of year, our work load and the type of building you are purchasing. At the time of your order and deposit, we will provide an approximate time frame for delivery. Give us a call any time to ask what our current lead times are for your location and structure!"
+    }, {
+        q: "WHAT IS THE DIFFERENCE BETWEEN BUYING A SHED IN STOCK AND ORDERING A SHED",
+        collapseVal: "collapse-8",
+        a: "There is no difference in the price of your structure! Depending on the lead time at your Sales Center, there may be a difference in delivery time, but not always. We're happy to deliver your dream shed direct from our sales center or fresh from our shop!"
+    }];
 
 });
